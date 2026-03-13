7d5e106831671e65e38946aaf44c55aaf15d8c9d
johannesdwicahyo <csk.rage@gmail.com>
2026-03-13 15:09:21 +0700

    feat: add dashboard layout with Tailwind navigation

---
+++ app/controllers/dashboard/base_controller.rb
@@ -0,0 +1,18 @@
+module Dashboard
+  class BaseController < ApplicationController
+    before_action :authenticate_user!
+    layout "dashboard"
+
+    private
+
+    def current_team
+      @current_team ||= current_user.teams.first
+    end
+    helper_method :current_team
+
+    def user_teams
+      @user_teams ||= current_user.teams
+    end
+    helper_method :user_teams
+  end
+end
