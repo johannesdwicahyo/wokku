3b7c1bb8321366606dd93fcb3291e573c96830bf
johannesdwicahyo <csk.rage@gmail.com>
2026-03-13 19:19:38 +0700

    feat: add marketing landing page and pricing

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  layout "application"
+
+  skip_before_action :verify_authenticity_token, only: []
+
+  def landing
+    if user_signed_in?
+      redirect_to dashboard_apps_path
+    else
+      render layout: "landing"
+    end
+  end
+
+  def pricing
+    @plans = Plan.order(:price_cents_per_month)
+  end
+
+  def docs
+  end
+end
