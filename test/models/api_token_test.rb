15a106baeba7a1ad410eb2bac3e23112d90be619
johannesdwicahyo <csk.rage@gmail.com>
2026-03-13 12:54:42 +0700

    feat: add ApiToken model with SHA256 hashing

--- test/models/api_token_test.rb
+++ test/models/api_token_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class ApiTokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create!(email: "test@example.com", password: "password123456")
+  end
+
+  test "create_with_token! returns token and plain text" do
+    token, plain = ApiToken.create_with_token!(user: @user, name: "test")
+    assert token.persisted?
+    assert_equal 64, plain.length
+  end
+
+  test "find_by_token finds active token" do
+    token, plain = ApiToken.create_with_token!(user: @user, name: "test")
+    found = ApiToken.find_by_token(plain)
+    assert_equal token.id, found.id
+  end
+
+  test "revoked token is not active" do
+    token, _ = ApiToken.create_with_token!(user: @user, name: "test")
+    token.revoke!
+    assert_not token.active?
+  end
+
+  test "expired token is not active" do
+    token, _ = ApiToken.create_with_token!(user: @user, name: "test", expires_at: 1.hour.ago)
+    assert_not token.active?
+  end
 end
