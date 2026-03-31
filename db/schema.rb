ba042a9b57740f9d09e5e15ad027bcd3cfbb1e34
johannesdwicahyo <csk.rage@gmail.com>
2026-03-31 09:40:13 +0700

    fix: use vCPU instead of CPU shares, fix metrics SSH to use server's configured user
    - CPU limits now use decimal vCPU values (0.5, 1.0, etc) instead of Docker shares
    - Metrics SSH uses server's ssh_user instead of hardcoded "root"
    - Display shows "0.5 vCPU" and "512 MB" with proper units

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_30_000001) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_31_023938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -184,7 +184,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_30_000001) do
   end
 
   create_table "dyno_tiers", force: :cascade do |t|
-    t.integer "cpu_shares", null: false
+    t.decimal "cpu_shares", precision: 4, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
     t.integer "memory_mb", null: false
     t.string "name", null: false
