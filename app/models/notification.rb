fca4e60615343cbc2cdd01144487803205e87fed
johannesdwicahyo <csk.rage@gmail.com>
2026-04-04 00:54:29 +0700

    feat: add expo-server-sdk gem and push channel to Notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :team
   belongs_to :app_record, optional: true
 
-  enum :channel, { email: 0, slack: 1, webhook: 2, discord: 3, telegram: 4 }
+  enum :channel, { email: 0, slack: 1, webhook: 2, discord: 3, telegram: 4, push: 5 }
 
   validates :channel, presence: true
   validates :events, presence: true
